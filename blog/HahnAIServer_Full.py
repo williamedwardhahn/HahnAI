33ea3e7cd88a17107d932cb0210b52b2cf5bf71d
hahn <williamedwardhahn@gmail.com>
2023-06-14 23:08:13 -0400

    update

--- blog/HahnAIServer_Full.py
+++ blog/HahnAIServer_Full.py
@@ -43,7 +43,7 @@ function generateNavigation() {
     let prevPage = baseURL + "blog" + prevPageNumber + ".html";
     let nextPage = baseURL + "blog" + nextPageNumber + ".html";
     let firstPage = baseURL + "blog.html";
-    let lastPage = baseURL + "blog{3}" + ".html"; //replace {last} with your last page number
+    let lastPage = baseURL + "blog{5}" + ".html"; //replace {last} with your last page number
 
     let navigationHTML = `
         <nav>
