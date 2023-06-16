37134674cfc25a1b5379d7319a616ca5cc8fc97d
hahn <williamedwardhahn@gmail.com>
2023-06-15 22:18:24 -0400

    update

--- blog/HahnAIServer_Index.py
+++ blog/HahnAIServer_Index.py
@@ -5,7 +5,63 @@
 #####################################################################################
 import pandas as pd
 import os
-from elements_index import *
+import pandas as pd
+from string import Template
+
+
+def get_database(url):
+    url_head = "https://docs.google.com/spreadsheets/d/"
+    url_foot = "/gviz/tq?tqx=out:csv&sheet="
+    url_body = url.split('/')[5]
+    sheet_name = "1"
+    url_csv = url_head + url_body + url_foot + sheet_name
+    df = pd.read_csv(url_csv, sep=',', skiprows=0)
+    return df
+    
+head = '''<!DOCTYPE HTML>
+<html>
+<head>
+<title>Hahn AI</title>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" href="assets/css/main.css" />
+</head>
+<body>
+<header id="header">
+<a href="index.html" class="logo"><strong>Hahn</strong> AI</a>
+</header>
+'''
+
+blog = '''
+<section id="main">
+<div class="inner">
+<a href = 'blog$i.html'><h4>$title</h4></a>
+<p><span class="image left"><a href = 'blog$i.html'><img src="$image" alt="" /></a> </span>$content <a href = 'blog$i.html'>Read More</a></p>
+'''
+
+blog_foot = '''				
+</div>
+</section>
+<br>
+'''
+
+foot = '''
+<footer id="footer">
+<div class="copyright">
+&copy; MPCR Lab <a href="https://mpcrlab.com">Main Site</a>.
+</div>
+</footer>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.scrolly.min.js"></script>
+<script src="assets/js/skel.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+</body>
+</html>'''
+
+
+blog = Template(blog)
+ 
 
 site_path = "/home/hahn/Desktop/HahnAI/blog/"    
 
