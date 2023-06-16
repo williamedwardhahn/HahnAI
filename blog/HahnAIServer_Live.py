bb79ad023d32b1d5ade4e258d4e420f34ecd471a
hahn <williamedwardhahn@gmail.com>
2023-06-15 22:37:57 -0400

    update

--- blog/HahnAIServer_Live.py
+++ blog/HahnAIServer_Live.py
@@ -8,6 +8,7 @@ import os
 import pandas as pd
 from string import Template
 
+
 def get_database(url):
     url_head = "https://docs.google.com/spreadsheets/d/"
     url_foot = "/gviz/tq?tqx=out:csv&sheet="
@@ -17,14 +18,6 @@ def get_database(url):
     df = pd.read_csv(url_csv, sep=',', skiprows=0)
     return df
     
-site_path = "/home/hahn/Desktop/HahnAI/blog/"    
-
-url_blog    = "https://docs.google.com/spreadsheets/d/12Aq-6jKjghX7TxJJPdAdVR8TfnImP5uZ2JhQYecjHK4/edit#gid=745755078"
-
-df_blog     = get_database(url_blog)    
-    
-    
-   
 head = '''<!DOCTYPE HTML>
 <html>
 <head>
@@ -45,8 +38,6 @@ blog = '''
 <a href = 'blog$i.html'><h4>$title</h4></a>
 <p><span class="image left"><a href = 'blog$i.html'><img src="$image" alt="" /></a> </span>$content <a href = 'blog$i.html'>Read More</a></p>
 '''
-
-blog = Template(blog)
 
 blog_foot = '''				
 </div>
@@ -69,9 +60,83 @@ foot = '''
 </html>'''
 
 
+blog = Template(blog)
+ 
+
+site_path = "/home/hahn/Desktop/HahnAI/blog/"    
+
+#Google sheet urls
+
+url_blog    = "https://docs.google.com/spreadsheets/d/12Aq-6jKjghX7TxJJPdAdVR8TfnImP5uZ2JhQYecjHK4/edit#gid=745755078"
+
+df_blog     = get_database(url_blog)
+
+source_blog = head #+ logo
+
+#####################################################################################
+
+for i in range(len(df_blog)):
+    
+    # if df_blog["Status"][i] == 1:
+        
+    photo_url = "https://drive.google.com/uc?export=download&id=" + str(df_blog["Post Image"][i]).split('=')[1]
+    
+    source_blog += blog.substitute(i = i, title=df_blog["Post Title"][i],image=photo_url,content=df_blog["Post Text"][i][:800]) + blog_foot 
+
+
+source_blog    += "<br><br><br><br><br><br>" +foot
+
+            
+print(source_blog,    file=open(site_path + "blog-1.html",    'w'))
+print(source_blog,    file=open(site_path + "index.html",    'w'))
+
+
+
+#####################################################################################
+#####################################################################################
+#####################################################################################
+
+    
+head = '''<!DOCTYPE HTML>
+<html>
+<head>
+<title>Hahn AI</title>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" href="assets/css/main.css" />
+</head>
+<body onload="generateNavigation()">
+<header id="header">
+<a href="index.html" class="logo"><strong>Hahn</strong> AI</a>
+</header>
+'''
+
+blog = '''
+<section id="main">
+<div class="inner">
+<h4>$title</h4>
+<p><span class="image left"><img src="$image" alt="" /></span>$content</p>
+'''
+
+blog_foot = '''				
+</div>
+</section>
+<br>
+'''
+
+blog    = Template(blog)
+
+site_path = "/home/hahn/Desktop/HahnAI/blog/"    
+
+#Google sheet urls
+
+url_blog    = "https://docs.google.com/spreadsheets/d/12Aq-6jKjghX7TxJJPdAdVR8TfnImP5uZ2JhQYecjHK4/edit#gid=745755078"
+
+df_blog     = get_database(url_blog)
+
 last = len(df_blog)-1
 
-foot_full = f'''
+foot = f'''
 <footer id="footer">
 <div class="copyright">
 &copy; Hahn AI <a href="https://Hahn.ai">Main Site</a>.
@@ -122,17 +187,15 @@ function generateNavigation() {{
 </html>'''
 
 
- 
-
-
-source_blog_index = head #+ logo
 
 #####################################################################################
 
 for i in range(len(df_blog)):
-    
-    source_blog = head #+ logo
 
+    source_blog = head #+ logo
+    
+    # if df_blog["Status"][i] == 1:
+        
     photo_url = "https://drive.google.com/uc?export=download&id=" + str(df_blog["Post Image"][i]).split('=')[1]
     
     source_blog += blog.substitute(title=df_blog["Post Title"][i],image=photo_url,content=df_blog["Post Text"][i]) + blog_foot 
@@ -140,20 +203,13 @@ for i in range(len(df_blog)):
     source_blog    += "<br><br><br>" +foot
         
     print(source_blog,    file=open(site_path + f"blog{i}.html",    'w'))
-  
-    source_blog_index += blog.substitute(i = i, title=df_blog["Post Title"][i],image=photo_url,content=df_blog["Post Text"][i][:800]) + blog_foot 
-
-
-source_blog_index    += "<br><br><br><br><br><br>" +foot
-
-print(source_blog_index,    file=open(site_path + "blog-1.html",    'w'))
-print(source_blog_index,    file=open(site_path + "index.html",    'w'))
     
     
 os.chdir(site_path)    
 os.system("git add . ")  
 os.system("git commit -m 'update'")  
 os.system("git push origin main ")    
+    
     
     
 
