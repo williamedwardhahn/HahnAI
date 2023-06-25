10c784d378a7b919d59fa391c1ca576afb3cc6e6
hahn <williamedwardhahn@gmail.com>
2023-06-25 17:33:38 -0400

    update

--- blog/update.py
+++ blog/update.py
@@ -2,6 +2,7 @@ import os
 site_path = "/home/hahn/Desktop/HahnAI/blog/"
 os.chdir(site_path)    
 os.system("git add . ")  
+os.system("git add .. ")  
 os.system("git commit -m 'update'")  
 os.system("git push origin main --force")   
 
