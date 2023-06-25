cb01d060d22e4ff9e3b982a894986cccc8002970
hahn <williamedwardhahn@gmail.com>
2023-06-25 17:20:18 -0400

    update

--- blog/HahnAIServer_Live2.py
+++ blog/HahnAIServer_Live2.py
@@ -228,7 +228,8 @@ for i in range(len(df_blog)):
 # Push to GitHub
 #####################################################################################    
 os.chdir(site_path)    
-os.system("git add . ")  
+os.system("git add . ")
+os.system("git add .. ")    
 os.system("git commit -m 'update'")  
 os.system("git push origin main ")    
     
