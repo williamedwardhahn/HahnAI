de0ac82ffa70432289a76510c5315c6b685c1044
William Edward Hahn <williamedwardhahn@gmail.com>
2023-10-26 22:14:19 -0400

    update

--- blog/HahnAIServer_Live.py
+++ blog/HahnAIServer_Live.py
@@ -233,7 +233,7 @@ os.chdir(site_path)
 os.system("git add . ")
 os.system("git add .. ")    
 os.system("git commit -m 'update'")  
-os.system("git push origin main ")    
+os.system("git push origin main -force")    
     
     
     
